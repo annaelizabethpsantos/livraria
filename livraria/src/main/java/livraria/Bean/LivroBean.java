b48e63680ea459b1253777ee07d307b0e6a82d46
Elizabeth <Elizabeth@LAPTOP-EC4L7MF2>
2021-07-20 21:15:17 -0300

    Versão 2.0

--- livraria/src/main/java/livraria/Bean/LivroBean.java
+++ livraria/src/main/java/livraria/Bean/LivroBean.java
@@ -15,14 +15,7 @@ public class LivroBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Livro livro = new Livro();
-	private String nomeBusca = "";
-
-	public void calculaMedia() {
-		double media = livro.getNota1() + livro.getNota2() + livro.getNota3();
-		media = media / 3;
-		livro.setMedia(media);
-	}
-	
+	private String nomeBusca = "";	
 
 	public Livro getLivro() {
 		return livro;
